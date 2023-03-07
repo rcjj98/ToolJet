aafceba95c2953418c2000e5f00fa7c318fa808f
Ryan Chang Jia Jie <>
2023-03-07 15:42:28 +0800

    Uncomment import statement to produce "Process object undefined" error

--- frontend/src/Editor/Components/OldRamcoTextbox.jsx
+++ frontend/src/Editor/Components/OldRamcoTextbox.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 //import DOMPurify from "dompurify";
-//import { NbTextbox } from "@nebula/rui-components";
+import { NbTextbox } from '@nebula/rui-components';
 //import { NbTextbox } from "@/_components/textbox/NbTextbox.tsx";
 
 // export default function OldRamcoTextbox() {
@@ -15,19 +15,19 @@ import React, { useState, useEffect, useCallback } from 'react';
 export const OldRamcoTextbox = () => {
   return (
     <>
-      {/* <NbTextbox
-            name={'id'}
-            //{...metaData?.commonProps}
-            caption={"text"}
-            //className={name}
-            disabled={false}
-            value={"hello world"}
-            //   onValueChange={()=>{}}
-            //   helpTaskName={metaData?.hasHelp ? metaData?.helpTask : undefined}
-            //   taskName={metaData?.commonProps?.task ? metaData?.commonProps?.task : undefined}
-            // onTask={() => { }}
-            // onEnter={() => { }}
-        />     */}
+      <NbTextbox
+        name={'id'}
+        //{...metaData?.commonProps}
+        caption={'text'}
+        //className={name}
+        disabled={false}
+        value={'hello world'}
+        //   onValueChange={()=>{}}
+        //   helpTaskName={metaData?.hasHelp ? metaData?.helpTask : undefined}
+        //   taskName={metaData?.commonProps?.task ? metaData?.commonProps?.task : undefined}
+        // onTask={() => { }}
+        // onEnter={() => { }}
+      />
     </>
   );
 };
